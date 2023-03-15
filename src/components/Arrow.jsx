f9ff592b4aaa04015770e9a9e6e19e7915cd5e54
Jean <nguyen.jean@me.com>
2023-03-15 16:18:57 +0100

    restore scroll function in arrow componant

--- src/components/Arrow.jsx
+++ src/components/Arrow.jsx
@@ -1,14 +1,18 @@
 import arrowDown from '../images/down-arrow.png';
 import { motion as m } from 'framer-motion';
 function Arrow() {
+  document.onscroll = () => {
+    const arrowDownEl = document.getElementById('arrow-down');
+    if (arrowDownEl) {
+      arrowDownEl.style.display = 'none';
+    }
+  };
   return (
     <m.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 1.0, fade: 'fadeIn', delay: 1 }}
-      whileInView='hidden'
-      viewport={{ once: true }}
+      transition={{ duration: 1.0, fade: 'fadeIn', delay: 1.5 }}
       id='arrow-down'
     >
       <img id='arrow-animation' src={arrowDown} alt='' />
